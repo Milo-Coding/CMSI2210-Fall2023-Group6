5f496c7005cc5d18b4ac398dbd19baa71cdf5e4e
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2023-09-02 17:59:39 -0700

    cleaning up line spacing

--- main/sayhello.c
+++ main/sayhello.c
@@ -4,14 +4,14 @@
 // says hello <name> to the user
 int main( int argc, char * argv[] ) {
     
-	// take the user input using scanf
+    // take the user input using scanf
     char input[25];
     printf("Enter Your name: ");
     scanf("%s", input);
 	
-	// say hello the user
+    // say hello the user
     printf("\n   Hello %s! \n ", input);
 	
-	// quits the program
+    // quits the program
     exit(0);  
 }
