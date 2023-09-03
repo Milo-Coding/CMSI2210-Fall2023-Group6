a9c942593fef13c347b6da46c6fb30acca0e8c1b
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2023-09-02 17:55:29 -0700

    Uploading part one of the homework 1

--- main/sayhello.c
+++ main/sayhello.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// says hello <name> to the user
 int main( int argc, char * argv[] ) {
-
-printf( "\n\n   Hello, world!\n\n" );
-
+    
+	// take the user input using scanf
+    char input[25];
+    printf("Enter Your name: ");
+    scanf("%s", input);
+	
+	// say hello the user
+    printf("\n   Hello %s! \n ", input);
+	
+	// quits the program
+    exit(0);  
 }
