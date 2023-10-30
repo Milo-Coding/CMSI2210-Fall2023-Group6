ded7209e60b0f6d3002e99c8c229269841c4a519
Milo-Coding <112501520+Milo-Coding@users.noreply.github.com>
2023-10-29 17:59:12 -0700

    wordcount.c code and file to test it

--- homework/homework05/wordcount.c
+++ homework/homework05/wordcount.c
@@ -8,4 +8,44 @@
  * while reading the file. You should also be able to handle files that are in
  * different directories from where your program resides
  */
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening the file");
+        return 1;
+    }
+
+    int wordCount = 0;
+    int inWord = 0;
+    char c;
+
+    while ((c = fgetc(file)) != EOF) {
+        if (c == ' ' || c == '\n' || c == '\t') {
+            if (inWord) {
+                inWord = 0;
+                wordCount++;
+            }
+        } else {
+            inWord = 1;
+        }
+    }
+
+    // Check for the last word
+    if (inWord) {
+        wordCount++;
+    }
+
+    fclose(file);
+
+    printf("Number of words in %s: %d\n", argv[1], wordCount);
+
+    return 0;
+}
